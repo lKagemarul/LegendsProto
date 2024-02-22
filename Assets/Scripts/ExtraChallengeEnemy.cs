b8a8a986d75758268574b340ef87b011f5687fa8
Danila <mylukin3@gmail.com>
2024-02-22 15:47:56 +0200

    edfgsdfg

---
+++ Assets/Scripts/ExtraChallengeEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraChallengeEnemy : MonoBehaviour
+{
+    /// <summary>
+    /// Contains tunable parameters to tweak the enemy's movement.
+    /// </summary>
+    [System.Serializable]
+    public struct Stats
+    {
+        [Header("Enemy Settings")] [Tooltip("How fast the enemy moves.")]
+        public float speed;
+
+        [Tooltip("Whether the enemy should move or not")]
+        public bool move;
+    }
+    
+    [Tooltip("The transform to which the enemy will pace back and forth to.")]
+    public Transform[] patrolPoints;
+    
+    public Stats enemyStats;
+
+    private int currentPatrolPoint = 0;
+    
+    void Update()
+    {
+        //if the enemy is allowed to move
+        if (enemyStats.move == true)
+        {
+            Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+            {
+                currentPatrolPoint++;
+
+                if (currentPatrolPoint > patrolPoints.Length)
+                {
+                    currentPatrolPoint = 0;
+                }
+
+            }
+        }
+
+    }
+}    
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    public static int health;
+
+    void Start()
+    {
+        // resets the score to 0 at the start of the game
+        score = 0;
+        health = 100;
+    }
+}
